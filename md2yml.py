33f67d93bc20f44e0a9552acac82d90115940575
Asheesh Laroia <asheesh@asheesh.org>
2017-11-06 19:25:53 -0800

    better yaml

--- md2yml.py
+++ md2yml.py
@@ -18,19 +18,21 @@ def name_transform(filename):
 def main():
     lines = sorted(
         [line.strip() for line in subprocess.check_output(['git', 'ls-files', '--', '*.md']).
-         decode('utf-8').split('\n') if line.strip()])
+         decode('utf-8').split('\n') if line.strip()],
+        key=lambda s: (s.count('/'), s),
+    )
     # Create a dict of dirs etc.
     created_prefixes = set()
     for line in lines:
         prefix = line.rsplit('/', 1)[0]
         if prefix == line:
             # No directory
-            print('-', name_transform(line) + ": '" + line + "'")
+            print('- ' + name_transform(line) + ": '" + line + "'")
         else:
             if prefix not in created_prefixes:
-                print(prefix + ':')
+                print('- ' + prefix + ':')
                 created_prefixes.add(prefix)
-            print('    -', name_transform(line) + ": '" + line + "'")
+            print('    - ' + name_transform(line) + ": '" + line + "'")
 
 if __name__ == '__main__':
     main()
